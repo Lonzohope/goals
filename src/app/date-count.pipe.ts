2ef8ec2cd728121689f41550eed130278264869e
maina murage <mainamurage21@gmail.com>
2019-03-26 12:22:37 +0300

    Add all files in a very confused manner because this was a learning app

---
+++ src/app/date-count.pipe.ts
@@ -0,0 +1,21 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'dateCount'
+})
+export class DateCountPipe implements PipeTransform {
+
+  transform(value: any, args?: any): number {
+    let today: Date = new Date(); //this gets current date and time
+    let todayWithNoTime : any = new Date(today.getFullYear(), today.getMonth(), today.getDate());
+    var dateDifference =Math.abs(value-todayWithNoTime); // returns the value in milliseconds
+    const secondsInADay= 86400; //60seconds*60minutes*24hours
+    var dateDifferenceSeconds=dateDifference*0.001; //converts to seconds
+    var dateCounter =dateDifferenceSeconds/secondsInADay;
+    if (dateCounter >=1 && value > todayWithNoTime) {
+      return dateCounter
+    } else {
+        return 0;
+    }
+  }
+}
---
+++ src/app/strikethrough.directive.ts
@@ -0,0 +1,21 @@
+import { Directive, ElementRef, HostListener } from '@angular/core';
+
+@Directive({
+  selector: '[appStrikethrough]'
+})
+export class StrikethroughDirective {
+
+  constructor(private elem:ElementRef) {
+    this.elem.nativeElement.style.textDecoration='none';
+   }
+   @HostListener("click") onclick(){
+     this.textDeco("line-through")
+   }
+   @HostListener("dblclick") onDoubleClick(){
+     this.textDeco("None")
+   }
+   private textDeco(action:string){
+     this.elem.nativeElement.style.textDecoration=action;
+   }
+
+}
